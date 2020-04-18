ee61648009ebbba198e10e68722b97dac7831c07
Brian Sutherland <bsuth701@gmail.com>
2020-04-18 13:43:21 +0200

    change port to support nodejs proxy server

--- src/kaos-server/index.js
+++ src/kaos-server/index.js
@@ -24,7 +24,7 @@ const path = require('path');
 // -----------------------------------------------------------------------------
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8081;
 const DIST = path.join(__dirname, 'public');
 
 
